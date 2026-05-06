c9bd51b70b3c95d7ae728455483fd9797c37056c
harsh.gupta <harsh.gupta@rapidshyp.com>
2026-05-06 12:20:08 +0530

    fix backend routing and remove frontend serving

--- backend/server.js
+++ backend/server.js
@@ -73,18 +73,9 @@ app.use('/api/v1/orders', orderRoutes);
 app.use('/api/v1/upload', uploadRoutes);
 app.use('/api/v1/payment', paymentRoutes);
 //-------------------------------------
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-  //any app route that is not api will redirected to index.html
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-} else {
-  app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello, World!');
-  });
-}
+});
 
 //-------------------------------------
 app.use(notFound);
