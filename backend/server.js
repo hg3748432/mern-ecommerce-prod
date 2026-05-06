74b1b93b188cab5e4df11d164e075fe71ecc7698
harsh.gupta <harsh.gupta@rapidshyp.com>
2026-05-06 11:45:08 +0530

    fix prom-client import

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
-const client = require('prom-client');
+import client from 'prom-client';
 
 // collect default metrics (CPU, memory etc)
 client.collectDefaultMetrics();
